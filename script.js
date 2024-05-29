901eb84e40f8b639e33c59a1e5a31bf5c8241880
fki-sz <167289583+fki-sz@users.noreply.github.com>
2024-05-29 17:13:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//JavaScript
+//console.log('Hello world!');
+// body の背景色を変える
+function MyFunction01() {
+    const obj = document.body;
+    obj.style.backgroundColor = 'lightsteelblue';
+}
+// ID要素 の背景色を変える
+function MyFunction02() {
+    const obj = document.getElementById('id01');
+    obj.style.backgroundColor = 'tomato';
+    obj.style.color ='white'
+}
+// CLASS要素 の背景色を変える
+function MyFunction03() {
+    const lines = document.getElementsByClassName('class01');
+    for (let i = 0; i < lines.length; i++) {
+        lines[i].style.backgroundColor = 'peru';
+    }
+}
+// HTML要素 の背景色を変える
+function MyFunction04() {
+    const obj = document.getElementsByTagName('p');
+    for (let i = 0; i < obj.length; i++) {
+        obj[i].style.backgroundColor = 'maroon';
+        obj[i].style.color ='white'
+    }
+}
